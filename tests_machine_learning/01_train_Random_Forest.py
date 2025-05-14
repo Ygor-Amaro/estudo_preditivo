30e8879816c4a480f2b4d50c2c2cf9720741d205
Ygor Kenji de Brito Amaro <ykba09@gmail.com>
2025-05-14 17:42:43 -0300

    random forest

--- tests_machine_learning/01_train_Random_Forest.py
+++ tests_machine_learning/01_train_Random_Forest.py
@@ -2,6 +2,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 from estudo_preditivo.transform_csv import dados
+from sklearn.ensemble import RandomForestClassifier
+
 
 print(dados.head())
 
